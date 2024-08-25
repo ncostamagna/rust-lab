77c36868979d7cdd9a95ac450ef76b7c57aee83b
Nahue <nlcostamagna@gmail.com>
2024-08-25 16:52:44 -0300

    3.3 functions, completed

--- examples/R02-common-programming-concepts/src/main.rs
+++ examples/R02-common-programming-concepts/src/main.rs
@@ -93,6 +93,20 @@ fn main() {
     another_function(5);
 
     print_labeled_measurement(5, 'm');
+
+    // Expression
+    let ey = {
+        let x = 3;
+        let y = 5;
+        x + 1 // no semicolon, return this value
+    };
+
+    println!("The value of y is: {ey}");
+
+
+    let frx = plus_one(5);
+
+    println!("The value of x is: {frx}");
 }
 
 // im rust we use snake case for function names
@@ -102,4 +116,8 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1 // no semicolon, return this value
 }
