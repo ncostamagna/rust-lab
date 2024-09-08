cd904b7a6dcc5ae32ec549c6b2f777e7ca586e3e
Nahue <nlcostamagna@gmail.com>
2024-09-08 13:38:12 -0300

    match

---
+++ examples/R062-enum-match-control/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    value_in_cents(Coin::Penny);
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    // with if we validate a boolean, with match we validate any value
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
