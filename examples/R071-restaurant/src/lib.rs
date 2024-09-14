83cb4c3045e014207849519e22b644415820007c
Nahue <nlcostamagna@gmail.com>
2024-09-14 15:37:52 -0300

    super, absolute and relative path

--- examples/R071-restaurant/src/lib.rs
+++ examples/R071-restaurant/src/lib.rs
@@ -1,6 +1,18 @@
+/*
+crate
+ └── front_of_house
+     ├── hosting
+     │   ├── add_to_waitlist
+     │   └── seat_at_table
+     └── serving
+         ├── take_order
+         ├── serve_order
+         └── take_payment
+*/
+
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+       pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -14,14 +26,21 @@ mod front_of_house {
     }
 }
 
-/*
-crate
- └── front_of_house
-     ├── hosting
-     │   ├── add_to_waitlist
-     │   └── seat_at_table
-     └── serving
-         ├── take_order
-         ├── serve_order
-         └── take_payment
-*/
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); // parent module, in this case back_of_house
+    }
+
+    fn cook_order() {}
+}
