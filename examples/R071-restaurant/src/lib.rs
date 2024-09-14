fe8146f9d7897999a657d33053fb3e198697f53b
Nahue <nlcostamagna@gmail.com>
2024-09-14 15:42:57 -0300

    7.3 done!

--- examples/R071-restaurant/src/lib.rs
+++ examples/R071-restaurant/src/lib.rs
@@ -37,10 +37,46 @@ pub fn eat_at_restaurant() {
 fn deliver_order() {}
 
 mod back_of_house {
+    
+    pub struct Breakfast { // public struct
+        pub toast: String, // public field
+        seasonal_fruit: String, // private field
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order(); // parent module, in this case back_of_house
     }
 
     fn cook_order() {}
+}
+
+pub fn eat_at_restaurant2() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
